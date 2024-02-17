19210882c2d39c7e7347bf5b743e7b35aef9950e
AnasCoding1986 <anassust.scw@gmail.com>
2024-02-17 06:49:56 +0600

    before matching key

--- script.js
+++ script.js
@@ -6,4 +6,14 @@ function play() {
 
 function continueGame() {
     generateRandomAlphabet();
+    setBackgroundColor();
 }
+
+document.addEventListener('keyup', function matchingKey(e) {
+    let pressedKey = e.key;
+    if (currentVal("randomAlphabet") === pressedKey) {
+        console.log("you got 1 number");
+    }else{
+        "you are wrong"
+    }
+});
--- utility.js
+++ utility.js
@@ -5,6 +5,8 @@
 // git remote add origin https://github.com/AnasCoding1986/alpha-clash-self.git
 // git push -u origin main
 
+// https://anascoding1986.github.io/alpha-clash-self/
+
 function addClass(id, addedClass) {
     let elm = document.getElementById(id);
     elm.classList.add(addedClass);
@@ -13,25 +15,21 @@ function removeClass(id, addedClass) {
     let elm = document.getElementById(id);
     elm.classList.remove(addedClass);
 }
+function currentVal(id) {
+    let elm = document.getElementById(id);
+    let elmText = elm.innerText;
+    return elmText;
+}
 function generateRandomAlphabet() {
     const alphabets = "abcdefghijklmnopqrstuvwxyz";
     const alphabetArr = alphabets.split("");
     let randomNum = Math.floor(Math.random()*26);
     let alphabet = alphabetArr[randomNum];
     document.getElementById("randomAlphabet").innerHTML = alphabet;
-    console.log(alphabet);
 }
 function setBackgroundColor() {
     let currentAlphabetDiv = document.getElementById("randomAlphabet");
     let currentAlphabetDivText = currentAlphabetDiv.innerText;
     let desiredCurrentAlphabetDivText = currentAlphabetDivText.toLowerCase();
-    const kbdElements = document.querySelectorAll('.kbd');
-    kbdElements.forEach(kbd => {
-        const id = kbd.id;
-        if (id === desiredCurrentAlphabetDivText) {
-            kbd.style.backgroundColor = 'yellow'; // Change background color
-        }
-    });
-
-
+    document.getElementById(desiredCurrentAlphabetDivText).classList.add("bg-color");
 }
